c7f0f9c70e33a471adeab3d868e899dcffa1a2a0
Alex Yong <alexjyong@gmail.com>
2025-08-11 13:10:30 -0400

    feature: Adding support for Github codespaces (#25)
    * Adding support for codespaces
    
    Signed-off-by: Alex Yong <alexjyong@gmail.com>
    
    * feat: allowing this to be ran in automated contexts
    
    Signed-off-by: Alex Yong <alexjyong@gmail.com>
    
    * Update devcontainer.json
    
    Signed-off-by: Alex Yong <alexjyong@gmail.com>
    
    * feat: Adding in dummy google services json file, and updating documentation
    
    * fix: typo :)
    
    * Adjusting storage bucket per feedback in Revolt Dev server.
    
    ---------
    
    Signed-off-by: Alex Yong <alexjyong@gmail.com>

--- scripts/download_deps.ts
+++ scripts/download_deps.ts
@@ -1,6 +1,5 @@
 import { resolve } from "jsr:@std/path"
 import { parseArgs } from "jsr:@std/cli/parse-args"
-import { ZipReader, Uint8ArrayReader, Uint8ArrayWriter } from "jsr:@zip-js/zip-js"
 
 const args = parseArgs(Deno.args, {
     boolean: ["yes", "y", "auto-accept"],
